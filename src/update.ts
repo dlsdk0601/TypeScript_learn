818e9d64be264abeae1744f38c0b5faaef84e0e7
inaJung <dlsdk0601@gmail.com>
2022-04-25 22:13:36 +0900

    never and unknown

--- src/update.ts
+++ src/update.ts
@@ -41,3 +41,38 @@ const player: readonly [string, number, boolean] = ["nico", 1, true];
 // any
 // 타입스크립트를 탈출하는 타입인데, 자주 쓰면 치명적이니 자주 쓰지말것.
 let a: any = [];
+
+// unknown;
+let a: unknown;
+
+if (typeof a === "number") {
+  let b = a + 1;
+}
+
+if (typeof a === "stirng") {
+  let b = a.toUpperCase();
+}
+
+// void
+// void는 자동으로 알기때문에 굳이 설정 필요 x
+function hello(): void {
+  console.log("x");
+}
+
+// never
+// 절대 return 하지 않을때
+
+function ex(): never {
+  throw new Error("xxxx");
+}
+
+function hi(name: string | number): never {
+  if (typeof name === "string") {
+    name;
+  } else if (typeof name === "number") {
+    name;
+  } else {
+    name;
+    // name이 숫자 아니면 문자라고 해놓고 예외를 두면 never로 잡힘
+  }
+}
